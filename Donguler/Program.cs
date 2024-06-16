38cc9ab8c533f9f5fb164697ecbb4e4fc8bf5cf7
Fatih SARICA <Fatih SARICA@DESKTOP-SQVTB1A>
2024-06-17 01:07:19 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        string kurs1 = "yazılım Geiştirme Kampı";
+        string kurs2 = "Programlamaya başlangıç için teöel kurs";
+        string kurs3 = "java";
+
+        //array - dizi
+
+        string[] kurslar = new string[] { "yazılım Geiştirme Kampı", "Programlamaya başlangıç için teöel kurs",
+            "java","python","c#" };
+
+
+        for (int i = 0; i < kurslar.Length  ; i++)
+        {
+            Console.WriteLine(kurslar[i]);
+        }
+
+        Console.WriteLine("for bitti");
+
+        foreach (string kurs in kurslar)
+        {
+            Console.WriteLine(kurs);
+        }
+
+
+        Console.WriteLine("sayfa sonu - footer");
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string katogoriEtiketi = "Katagoriler";
+        int ogrenciayisi = 32000;
+        double faizOranı = 1.45;
+        bool sistemeGrisYapmısMı = true;
+        double dolarDun = 7.55;
+        double dolarBugun = 7.45;
+
+        if (dolarDun>dolarBugun)
+        {
+            Console.WriteLine("Azalış Butonu");
+        }
+        else if (dolarDun<dolarBugun)
+        {
+            Console.WriteLine("Artış butonu");
+            
+        }
+        else
+        {
+            Console.WriteLine("değişmedi butonu");
+        }
+
+
+        if (sistemeGrisYapmısMı== true)
+        {
+            Console.WriteLine("Kullanıcı ayarları butonu");
+        }
+        else
+        {
+            Console.WriteLine("Giriş yap butonu");
+        }
+
+
+
+
+        Console.WriteLine (katogoriEtiketi);
+        
+
+
+    }
+}
